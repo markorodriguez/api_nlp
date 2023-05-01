f1abb6a9751401b2c28d74030bddd3872ae8a685
unknown <marko.rodriguez@hotmail.es>
2023-05-01 01:07:37 -0500

    port env added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 const {training, getTrainedResponse} = require('./training')
 const sentiment = require('./sentiment')
